37169428161e9ff9a2f39d1c6a61410d410402c4
Damian Ly <damian@rayatheapp.com>
2021-11-23 13:51:50 -0800

    added test structure

---
+++ backend/test/end-to-end/e2e.js
@@ -0,0 +1,11 @@
+/* describe */
+
+const chai = require('chai');
+
+const { expect } = chai;
+
+describe('smoke test', () => {
+  it('checks equality', async () => {
+    expect(true).to.be.true;
+  });
+});
